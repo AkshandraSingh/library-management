35048cb2255e6a9b31cb46f76e3b6bbfdb5563a8
Ishan Singh <136164702+ishansingh1010@users.noreply.github.com>
2023-09-23 22:09:19 +0530

    addingAdminAPI

--- admin/controllers/adminController.js
+++ admin/controllers/adminController.js
@@ -23,6 +23,70 @@ const adminDashBoard = async (req, res) => {
     }
 }
 
+const viewUsers = async (req, res) => {
+    try {
+        const usersData = await userModel.find({}).select('userName userPhone userEmail userProfilePic')
+        res.status(200).send({
+            success: true,
+            message: "All Uses Data",
+            usersData: usersData,
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error!",
+            error: error.message
+        })
+    }
+}
+
+const userDetails = async (req, res) => {
+    try {
+        const { userId } = req.params
+        const userData = await userModel.findById(userId).select('userName userPhone userEmail userProfilePic userGender borrowBooks')
+        if (userData) {
+            res.status(200).send({
+                success: true,
+                message: "User data found!",
+                userData: userData
+            })
+        } else {
+            res.status(400).send({
+                success: false,
+                message: "User not found!",
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error!",
+            error: error.message
+        })
+    }
+}
+
+const borrowBooksList = async (req, res) => {
+    try {
+        const borrowBooks = await bookModel.find({
+            bookStatus: "not available"
+        }).select('bookName bookAuthor bookCategory bookImage bookCost')
+        res.status(200).send({
+            success: true,
+            message: "Borrow books list",
+            books: borrowBooks
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error!",
+            error: error.message
+        })
+    }
+}
+
 module.exports = {
     adminDashBoard,
+    viewUsers,
+    userDetails,
+    borrowBooksList,
 }
--- admin/routes/adminRoutes.js
+++ admin/routes/adminRoutes.js
@@ -9,5 +9,8 @@ const adminRoutes = express.Router()
 
 adminRoutes.post('/adminLogin', authService.isAdmin, userController.loginUser)
 adminRoutes.get('/adminDashBoard', userAuthentication, adminController.adminDashBoard)
+adminRoutes.get('/viewUsers', userAuthentication, adminController.viewUsers)
+adminRoutes.get('/userDetails/:userId', userAuthentication, adminController.userDetails)
+adminRoutes.get('/borrowBooksList', userAuthentication, adminController.borrowBooksList)
 
 module.exports = adminRoutes
